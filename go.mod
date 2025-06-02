c702601cb60e279ffdb8375bb144b1f6bf2932f9
Abtin <abtinghazavy@yahoo.com>
2025-06-02 23:14:07 +0200

    fixed the issue

--- go.mod
+++ go.mod
@@ -2,15 +2,15 @@ module github.com/Abtingh/Zeiterfassung
 
 go 1.24.2
 
-require github.com/spf13/viper v1.20.1
+require (
+	github.com/jackc/pgx/v5 v5.7.5
+	github.com/spf13/viper v1.20.1
+)
 
 require (
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
-	github.com/jackc/pgx/v5 v5.7.5 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
-	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.19 // indirect
 	golang.org/x/crypto v0.37.0 // indirect
 	golang.org/x/sync v0.13.0 // indirect
 )
@@ -19,7 +19,6 @@ require (
 	github.com/fsnotify/fsnotify v1.8.0 // indirect
 	github.com/go-viper/mapstructure/v2 v2.2.1 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
-	github.com/rs/zerolog v1.34.0
 	github.com/sagikazarmark/locafero v0.7.0 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
 	github.com/spf13/afero v1.12.0 // indirect
--- internal/api/handlers/login.go
+++ internal/api/handlers/login.go
@@ -1,10 +1,4 @@
-package api
-
-import (
-	"fmt"
-	"log"
-	"net/http"
-)
+package handlers
 
 type LoginRequest struct {
 	Email    string `json:"email"`
@@ -17,36 +11,3 @@ type LoginResponse struct {
 }
 
 var jwtSecretKey []byte
-
-func LoginHandler(w http.ResponseWriter, r *http.Request) {
-	// Log the incoming request
-	log.Printf("→ %s %s", r.Method, r.URL.Path)
-
-	if r.Method == http.MethodGet {
-		http.ServeFile(w, r, "./public/public-static/login.html")
-		return
-	}
-
-	if r.Method == http.MethodPost {
-		// Parse form data
-		if err := r.ParseForm(); err != nil {
-			http.Error(w, fmt.Sprintf("ParseForm() error: %v", err), http.StatusBadRequest)
-			return
-		}
-
-		// Extract form values
-		email := r.FormValue("email")
-		password := r.FormValue("password")
-
-		// Write response
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		fmt.Fprintf(w, "POST request successful\n")
-		fmt.Fprintf(w, "Email: %s\n", email)
-		fmt.Fprintf(w, "Password: %s\n", password)
-		return
-	}
-}
-
-func LogoutHandler(w http.ResponseWriter, r *http.Request) {}
-
-func RegisterHandler(w http.ResponseWriter, r *http.Request) {}
